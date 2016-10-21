986a46ececb2559f005d3b0984c82f9000fef355
A C <andy@vtwebshop.com>
2016-10-21 10:17:51 -0400

    bootstrapped app

---
+++ app/tag-editor/expenseListDisplay.js
@@ -0,0 +1,9 @@
+angular.module('app').component('tagEditor', {
+  bindings: {
+    // expenses: '=expenseData'
+  },
+  templateUrl: './tag-editor/expenseListDisplay.html',
+  controller: function() {
+
+  }
+})
