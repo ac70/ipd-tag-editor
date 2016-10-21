fa661727bf664d37773f4a9397c3dbf2ea8516fb
A C <andy@vtwebshop.com>
2016-10-21 19:14:58 -0400

    basics working

--- app/app.js
+++ app/app.js
@@ -1,1 +1,41 @@
-var app = angular.module('app', ['firebase', 'ngMaterial']);
+var app = angular
+  .module('app', ['firebase', 'ngMaterial'])
+  .config(function($mdThemingProvider) {
+    $mdThemingProvider
+      .theme('default')
+      .primaryPalette('blue-grey')
+      .accentPalette('deep-orange')
+      .warnPalette('red')
+      .backgroundPalette('blue-grey');
+    })
+    // .controller('appController', function($scope, $firebaseObject) {
+    //
+    //     var tagsDB = firebase.database().ref('groups/groupkey1/tags');
+    //
+    //     // sync as object
+    //     var syncObject = $firebaseObject(tagsDB);
+    //
+    //     // three way data binding
+    //     syncObject.$bindTo($scope, 'tags');
+    //
+    //     $scope.reset = function() {
+    //       console.log('reset test data');
+    //         // WRITING TO FIREBASE DB
+    //         tagsDB.set({
+    //                     tags: {
+    //                         tagkey1: {
+    //                             name: 'tag1fghj',
+    //                             order: '1'
+    //                         },
+    //                         tagkey2: {
+    //                             name: 'tag2',
+    //                             order: '2'
+    //                         },
+    //                         tagkey3: {
+    //                             name: 'tag3',
+    //                             order: '3'
+    //                         }
+    //                     }
+    //             });
+    //     }
+    // });
--- app/tag-editor/tag-editor.js
+++ app/tag-editor/tag-editor.js
@@ -1,7 +1,56 @@
 angular.module('app').component('tagEditor', {
 
   templateUrl: 'tag-editor/tag-editor.html',
-  controller: function() {
+  bindings: {
+    tags: '='
+  },
+  controller: function($scope, $firebaseObject, $firebaseArray) {
 
-  }
-})
+      var tagsDB = firebase.database().ref('groups/groupkey1');
+
+      // sync as object
+      var syncObject = $firebaseObject(tagsDB);
+
+      // three way data binding
+      syncObject.$bindTo($scope, '$ctrl.group');
+
+      this.deleteTag = function(id) {
+        var tagRef = firebase.database().ref('groups/groupkey1/tags/'+id);
+        var tagObject = $firebaseObject(tagRef);
+        tagObject.$remove();
+      };
+
+      this.addNewTag = function() {
+        var tagsRef = firebase.database().ref('groups/groupkey1/tags/');
+        var tagsObject = $firebaseArray(tagsRef);
+
+        tagsObject.$add({ name: this.newTag }).then((function(ref){
+          this.newTag = "";
+        }).bind(this));
+      }
+
+      // function to set the default data
+      $scope.reset = function() {
+        console.log('reset test data');
+          // WRITING TO FIREBASE DB
+          tagsDB.set({
+                        tags: {
+                          tagkey1: {
+                              name: 'tag1',
+                              order: '1'
+                          },
+                          tagkey2: {
+                              name: 'tag2',
+                              order: '2'
+                          },
+                          tagkey3: {
+                              name: 'tag3',
+                              order: '3'
+                          }
+                        }
+              });
+      }
+
+
+    }
+});
